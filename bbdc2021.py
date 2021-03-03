bbbab8c79fd402a07d71a3d9bb53b8a7ab2918e0
Jannes Adam <jadam@uni-bremen.de>
2021-03-03 21:47:11 +0100

    - Fehler beim Daten laden behoben (Zu jeder Datei wurden mehrere Arrays erzeugt) - Split-Methode zur Einteilung in train, validation, test

--- bbdc2021.py
+++ bbdc2021.py
@@ -10,7 +10,7 @@ General helper functions for the Bremen Big Data Challenge 2021
 import pandas as pd
 import numpy as np
 
-def read_csv_data(pathToDataset): #TODO Val und Test abspalten
+def read_train(pathToDataset): #TODO Val und Test abspalten
     """Liest Trainingsdaten inklusive Label ein"""
     trainLabels = pd.read_csv(pathToDataset+"/dev-labels.csv") 
     trainLabelsOneHot = pd.get_dummies(trainLabels['event_label'])
@@ -22,6 +22,7 @@ def read_csv_data(pathToDataset): #TODO Val und Test abspalten
     classNum = 13
     for index, row in trainLabels.iterrows():
         if row["filename"]!=currentFile:
+            currentFile = row["filename"]
             features = np.genfromtxt(pathToDataset+"/dev/"+row["filename"].replace(".wav", ".csv"),delimiter=',')
             X_train.append(features[:,1:])
             y = np.zeros((timepoints.size, classNum))
@@ -29,3 +30,14 @@ def read_csv_data(pathToDataset): #TODO Val und Test abspalten
             Y_train.append(y)
         Y_train[-1][np.where(np.logical_and(timepoints>=row["onset"], timepoints<=row["offset"])),:] = trainLabelsOneHot.iloc[index].values
     return X_train, Y_train    
+
+def splitTrain(X_train, Y_train, trainPortion=0.8, valPortion=0.1):
+    firstIdx = int(len(X_train)*trainPortion)
+    secondIdx = int(len(X_train)*valPortion)+firstIdx
+    X_validation = X_train[firstIdx:secondIdx]
+    Y_validation = Y_train[firstIdx:secondIdx]
+    X_test = X_train[secondIdx:]
+    Y_test = Y_train[secondIdx:]
+    X_train = X_train[:firstIdx]
+    Y_train = Y_train[:firstIdx]
+    return X_train, Y_train, X_validation, Y_validation, X_test, Y_test
