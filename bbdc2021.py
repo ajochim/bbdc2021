65cb19adfee81471479367f7d07869dd63f74933
ajochim <ajochim@uni-bremen.de>
2021-03-03 13:29:07 +0100

    first commit

---
+++ bbdc2021.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 3 13:21:17 2021
+
+@author: Jannes, Max, Alex
+
+General helper functions for the Bremen Big Data Challenge 2021
+"""
+
+def read_data():
+    """Description"""
+    pass
