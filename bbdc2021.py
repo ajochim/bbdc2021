d98e59a156bf2f434110856674117bc25ebeb0d9
Jannes Adam <jadam@uni-bremen.de>
2021-03-06 20:58:16 +0100

    Methode, um Vorhersage in Sequenzen zu transformieren

--- bbdc2021.py
+++ bbdc2021.py
@@ -11,8 +11,11 @@ import pandas as pd
 import numpy as np
 import keras.backend as K
 import tensorflow as tf
+from itertools import groupby
+from operator import itemgetter
 
 LABEL_DICT =  {'Noise': 0, 'Bark': 1, 'Burping_and_eructation': 2, 'Camera':3, 'Cheering':4, 'Church_bell':5, 'Cough':6, 'Doorbell':7, 'Fireworks':8, 'Meow':9, 'Scratching_(performance_technique)':10, 'Shatter':11, 'Shout':12}
+invLabelMap = {v: k for k, v in LABEL_DICT.items()}
 
 
 def load_data(fileListName, datasetName, pathToDataDir="./../data/"):
@@ -45,12 +48,28 @@ def dice_coef(y_true, y_pred, smooth=1):
          =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
     ref: https://arxiv.org/pdf/1606.04797v1.pdf
     """
-    intersection = K.sum(y_true * y_pred, axis=-1)
-    denominator = K.sum(y_true, axis=-1) + K.sum(y_pred, axis=-1)+smooth
-    return tf.math.divide_no_nan(denominator - (2.*intersection+smooth), denominator)
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
     # Alternative implementation
-    #intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
-    #return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
+    #intersection = K.sum(y_true * y_pred, axis=-1)
+    #denominator = K.sum(y_true, axis=-1) + K.sum(y_pred, axis=-1)+smooth
+    #return tf.math.divide_no_nan(denominator - (2.*intersection+smooth), denominator)
 
 def dice_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
+
+def getPredictionAsSequenceDF(prediction, timepoints, dataframeWithFiles):
+    y_predicted = np.argmax(prediction, axis=2)
+    detectedEvents = []
+    for fileNumber in range(len(y_predicted)):
+        pred = y_predicted[fileNumber]
+        predAndTime = np.zeros((len(pred),2))
+        predAndTime[:,0] = pred
+        predAndTime[:,1] = timepoints[1:]
+        groups = [list(group) for key, group in groupby(predAndTime, itemgetter(0))]
+        for group in groups:
+            firstTime = group[0][1]
+            lastTime = group[-1][1]
+            key = int(group[0][0])
+            detectedEvents.append([dataframeWithFiles.iloc[fileNumber]["filename"], firstTime, lastTime, invLabelMap[key]]) 
+    return pd.DataFrame(detectedEvents, columns=["filename", "onset", "offset", "event_label"])
