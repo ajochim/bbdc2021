3308a7ddfc1abc3bd4caa8761a2db98d163c0bdd
Jannes Adam <jadam@uni-bremen.de>
2021-03-11 10:33:00 +0100

    merged less-parameter-u-net in u-net

--- models/cnn/u_net_1d.py
+++ models/cnn/u_net_1d.py
@@ -9,30 +9,41 @@ def conv_bn_relu_block(x, numChannels, padding):
     x = layers.ReLU()(x)
     return x
 
+def conv_bn_relu_block_lessParameter(x, numChannels, padding):
+    x = layers.Conv1D(numChannels, kernel_size=1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv1D(numChannels, kernel_size=3, padding = padding)(x)
+    return x
 
-def u_net(inputShape, channels, padding = "same", activation="softmax", numClasses = 13):
+
+def u_net(inputShape, channels, padding = "same", activation="softmax", numClasses = 13, lessParameter=False):
     if padding != "same":
         print("Gibt bisher nur Same-Padding")
         return
     input_layer = layers.Input(shape=(inputShape))
+    conv_block_function = conv_bn_relu_block
+    if lessParameter:
+        conv_block_function = conv_bn_relu_block_lessParameter
+    
     shortcuts = []
     x = input_layer
     #Encoder
     for numLayer in range(len(channels)-1):
-        x = conv_bn_relu_block(x, channels[numLayer], padding)
-        x = conv_bn_relu_block(x, channels[numLayer], padding)
+        x = conv_block_function(x, channels[numLayer], padding)
+        x = conv_block_function(x, channels[numLayer], padding)
         shortcuts.append(x)
         x = layers.MaxPooling1D()(x)
         
-    x = conv_bn_relu_block(x, channels[numLayer], padding)
-    x = conv_bn_relu_block(x, channels[numLayer], padding)
+    x = conv_block_function(x, channels[numLayer], padding)
+    x = conv_block_function(x, channels[numLayer], padding)
     
     #Decoder start
     for numLayer in reversed(range(len(channels)-1)):
         x = layers.UpSampling1D(2)(x)
         x = layers.Concatenate()([x,shortcuts[numLayer]])
-        x = conv_bn_relu_block(x, channels[numLayer], padding)
-        x = conv_bn_relu_block(x, channels[numLayer], padding)
+        x = conv_block_function(x, channels[numLayer], padding)
+        x = conv_block_function(x, channels[numLayer], padding)
 
     x = layers.Conv1D(numClasses, kernel_size=1, activation=activation, padding=padding)(x)
 
